f7155a355494649729f7eb273668224d2c9e5396
root <root@DESKTOP-1PF4AT3.localdomain>
2022-10-05 08:41:44 +0900

    deploy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use yew::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    yew::start_app::<App>();
+}
+
+#[function_component(App)]
+pub fn app() -> Html {
+    html! {
+        <main>
+            <img class="logo" src="https://yew.rs/img/logo.png" alt="yew logo" />
+            <span class="subtitle">{ "from yew"}<i class="heart" /></span>
+        </main>
+    }
 }
